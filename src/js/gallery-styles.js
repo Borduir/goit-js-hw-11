07d2d15fbfa9480e687700ad27423bbaa7d07899
Denis Smolianyi <denis.smolyanoj@gmail.com>
2022-10-03 20:43:11 +0300

    Update gallery-styles.js

--- src/js/gallery-styles.js
+++ src/js/gallery-styles.js
@@ -1,6 +1,6 @@
 const createGalleryStyles = () => {
   const galleryCards = document.querySelectorAll('.photo-card');
-  for (card of galleryCards) {
+  for (const card of galleryCards) {
     card.style.border = '2px solid black';
     card.style.margin = '20px 10px 10px 0';
     card.style.color = 'tomato';
